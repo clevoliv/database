0aba8b64df2cdbe4fffaeba8c9e3c3853c5ee8d3
clevoliv <cleversonoliveira@gmail.com>
2022-03-14 16:54:15 -0300

    initial

---
+++ dio.sql
@@ -0,0 +1,10 @@
+CREATE TABLE pessoas (
+id INT NOT NULL PRIMARY KEY AUTOINCREMENT,
+pessoa VARCHAR(30) NOT NULL,
+nascimento DATE
+
+)
+
+INSERT INTO pessoas (nome, nascimento) VALUES ('Nathally', '1990-05-22')
+INSERT INTO pessoas (nome, nascimento) VALUES ('Joel', '1994-07-20')
+INSERT INTO pessoas (nome, nascimento) VALUES ('Ligia', '1985-08-10')
